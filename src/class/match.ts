28c077268c77484cd34a82a3520f912f8daa18b5
CaptainBidou <tomas.salvado_robalo@ig2i.centralelille.fr>
2023-10-26 11:07:21 +0200

    match

---
+++ src/class/match.ts
@@ -0,0 +1,23 @@
+// const { DataTypes } = require('sequelize');
+// const sequelize = require('../db.js');
+
+// const Matches = sequelize.define('Matches', {
+//     id: {
+//       type: DataTypes.INTEGER,
+//       primaryKey: true,
+//     },
+//     uid1: DataTypes.INTEGER,
+//     uid2: DataTypes.INTEGER,
+//     date: DataTypes.DATE,
+//   });
+
+// module.exports = Matches;
+
+export class Match {
+
+    constructor(id: number, uid1: number, uid2: number, date: Date) {
+
+    }
+
+
+}
